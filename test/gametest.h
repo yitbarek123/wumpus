2ab4e7324983e04e67a50ff6314886cc4355b368
yitbarek <yitbarek_yohannes@yahoo.com>
2019-09-14 17:07:51 +0300

    Writing algorithm for the game

---
+++ test/gametest.h
@@ -0,0 +1,26 @@
+//
+// Created by yitbarek on 9/14/19.
+//
+
+#include <cxxtest/TestSuite.h>
+#include "../src/game.h"
+class gametest : public CxxTest::TestSuite
+{
+public:
+    void testgamefailled( void )
+    {
+        wumpus t;
+        int game[4][4];
+
+        for(int i=0;i<4;i++) {
+            for(int j=0;j<4;j++){
+                game[i][j]=0;
+            }
+        }
+        game[2][0]=-2;
+        game[0][2]=-1;
+        game[3][1]=-1;
+        game[3][3]=100;
+        TS_ASSERT_EQUALS( t.check_failed(0,2,game), true );
+    }
+};
