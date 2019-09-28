819fdacfdc4cf76af07dc89511e44cf156f4c2d9
yitbarek <yitbarek_yohannes@yahoo.com>
2019-09-28 16:03:15 +0300

    add cmake-build to gitignore
    edit git ignore
    
    edit git ignore
    
    remove unneccasary files
    
    delete unneccasary files

--- cmake-build-debug/tests/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/yitbarek/Desktop/experment/Project/wumpus/tests
-# Build directory: /home/yitbarek/Desktop/experment/Project/wumpus/cmake-build-debug/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
