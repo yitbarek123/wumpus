d0a2a116da7eaaf44684f9c483cfd9f13136a656
yitbarek <yitbarek_yohannes@yahoo.com>
2019-09-28 13:03:10 +0300

    add delaye

---
+++ cmake-build-debug/CMakeFiles/gameunittest.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gameunittest.dir/tests/gameunittest.cpp.o"
+  "gameunittest.pdb"
+  "gameunittest"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gameunittest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
